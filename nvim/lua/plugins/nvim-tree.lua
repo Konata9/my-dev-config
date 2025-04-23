2715ec59219ed732e483eaa284e3950854e16525
chichen <chichen.mao@assaabloy.com>
2025-04-23 17:02:28 +0800

    Update nvim-tree options

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -4,12 +4,22 @@ return {
     keys = {
       {"<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Toggle file tree"},
     },
+    init = function()
+      vim.api.nvim_create_autocmd({"VimEnter", "DirChanged"}, {
+          callback = function()
+              if vim.fn.argc() == 0 then
+                  require("nvim-tree.api").tree.open()
+              end
+          end,
+      })
+    end,
     config = function()
         local function on_attach(bufnr)
           local api = require("nvim-tree.api")
           local opts = { buffer = bufnr, noremap = true, silent = true, nowait = true }
 
           vim.keymap.set("n", "l", api.node.open.edit, opts)
+          vim.keymap.set("n", "o", api.node.open.edit, opts)
           vim.keymap.set("n", "h", api.node.navigate.parent_close, opts)
           vim.keymap.set("n", "s", api.node.open.vertical, opts)
         end
