a62dc9787b16941d1cae85da5b34b708c9d162c4
konata9 <maochichen@163.com>
2025-04-23 23:07:16 +0800

    Update plugins

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,8 +2,14 @@ return {
   "catppuccin/nvim", 
   name = "catppuccin", 
   priority = 1000,
+  opts = {
+    flavour = "macchiato", 
+    integrations = {
+      bufferline = true, 
+    },
+  },
   config = function()
     -- Load the scheme when nvim start
-    vim.cmd("colorscheme catppuccin-macchiato")
+    vim.cmd("colorscheme catppuccin")
   end,
 }
