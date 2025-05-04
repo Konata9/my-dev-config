bfeccb510616b8d22a63cbf2ed86709509588d78
konata9 <maochichen@163.com>
2025-05-04 22:22:23 +0800

    Add mini.nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-  "catppuccin/nvim", 
-  name = "catppuccin", 
+  "catppuccin/nvim",
+  name = "catppuccin",
   priority = 1000,
   opts = {
-    flavour = "macchiato", 
+    flavour = "macchiato",
     integrations = {
-      bufferline = true, 
+      bufferline = true,
     },
   },
   config = function()
---
+++ nvim/lua/plugins/mini.lua
@@ -0,0 +1,10 @@
+-- Groups of mini.nvim plugins needed
+return {
+  { 'echasnovski/mini.statusline',  version = false, config = true },
+  { 'echasnovski/mini.hipatterns',  version = false, config = true},
+  { 'echasnovski/mini.cursorword',  version = false, config = true },
+  { 'echasnovski/mini.tabline',     version = false, config = true },
+  { 'echasnovski/mini.trailspace',  version = false, config = true },
+  { 'echasnovski/mini.indentscope', version = false, config = true },
+  { 'echasnovski/mini.animate',     version = false, config = true },
+}
