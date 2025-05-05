8354d46425d33b14a80e949949c6d64909f808e2
konata9 <maochichen@163.com>
2025-05-05 17:08:00 +0800

    Update mini-status

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,8 +1,15 @@
 -- Groups of mini.nvim plugins needed
 return {
-  { 'echasnovski/mini.statusline',  version = false, config = true },
-  { 'echasnovski/mini.hipatterns',  version = false, config = true},
   { 'echasnovski/mini.cursorword',  version = false, config = true },
+  {
+    'echasnovski/mini.statusline',
+    version = false,
+    dependecies = {
+      "nvim-tree/nvim-web-devicons"
+    },
+    config = true
+  },
+  { 'echasnovski/mini.hipatterns',  version = false, config = true },
   { 'echasnovski/mini.tabline',     version = false, config = true },
   { 'echasnovski/mini.trailspace',  version = false, config = true },
   { 'echasnovski/mini.indentscope', version = false, config = true },
