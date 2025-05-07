6cab9a61f386c1a8aa43c55f6ebc1825b7c07ce5
chichen <chichen.mao@assaabloy.com>
2025-05-07 16:38:45 +0800

    update

--- plugins/nvim-lspconfig.lua
+++ plugins/nvim-lspconfig.lua
@@ -2,8 +2,9 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		opts = {
-			-- Just disable autoformat
-			autoformat = false,
+      diagnostics = {
+        virtual_text = false,
+      },
 		},
 	},
 }
