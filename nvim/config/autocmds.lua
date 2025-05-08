5955108b6c858850e99129a8257ef64f93fceff3
chichen <chichen.mao@assaabloy.com>
2025-05-08 18:08:59 +0800

    Add hbr files highlight

--- nvim/config/autocmds.lua
+++ nvim/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+-- Treat handlebar as html
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "*.hbr",
+  callback = function()
+    vim.bo.filetype = "html"
+  end,
+})
