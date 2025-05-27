de81fbbfbd0ca7f274cf748fa47172a54e2d6049
chichen <chichen.mao@assaabloy.com>
2025-05-27 15:13:17 +0800

    Add codeium and yazi

--- custom/nvim/lua/plugins/blink.lua
+++ custom/nvim/lua/plugins/blink.lua
@@ -1,6 +1,10 @@
 return {
   'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
+
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    'Exafunction/codeium.nvim',
+  },
   -- use a release tag to download pre-built binaries
   version = '1.*',
   event = "InsertEnter",
@@ -17,8 +21,8 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { 
-      preset = 'super-tab' ,
+    keymap = {
+      preset = 'super-tab',
 
       ['<C-/>'] = { 'show', 'show_documentation', 'hide_documentation' },
       ['<C-k>'] = { 'select_prev', 'fallback' },
@@ -28,19 +32,22 @@ return {
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- Adjusts spacing to ensure icons are aligned
-      
+
       nerd_font_variant = 'mono'
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { 
-      documentation = { auto_show = false } 
+    completion = {
+      documentation = { auto_show = false }
     },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'codeium' },
+      providers = {
+        codeium = { name = 'Codeium', module = 'codeium.blink', async = true },
+      },
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
