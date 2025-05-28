008ed082670cae445ab26a0a8e87c7a3f6145bcd
chichen <chichen.mao@assaabloy.com>
2025-05-28 14:45:25 +0800

    set catppuccin integration

--- custom/nvim/lua/plugins/catppuccin.lua
+++ custom/nvim/lua/plugins/catppuccin.lua
@@ -5,7 +5,35 @@ return {
   name = "catppuccin",
   priority = 1000,
   opts = {
-    integrations = { blink_cmp = true },
+    integrations = {
+      gitsigns = true,
+      indent_blankline = {
+        enabled = true,
+        scope_color = "", -- catppuccin color (eg. `lavender`) Default: text
+        colored_indent_levels = false,
+      },
+      markdown = true,
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
+          ok = { "italic" },
+        },
+        underlines = {
+          errors = { "underline" },
+          hints = { "underline" },
+          warnings = { "underline" },
+          information = { "underline" },
+          ok = { "underline" },
+        },
+        inlay_hints = {
+          background = true,
+        },
+      },
+    },
   },
   config = function()
     -- load the colorscheme here
