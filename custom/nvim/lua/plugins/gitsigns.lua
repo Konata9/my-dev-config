d306700e88e6ae3551586349e34d4325b5d6ce29
chichen <chichen.mao@assaabloy.com>
2025-06-03 16:03:48 +0800

    Enable blame line

--- custom/nvim/lua/plugins/gitsigns.lua
+++ custom/nvim/lua/plugins/gitsigns.lua
@@ -7,6 +7,7 @@ return {
       change = { text = "~" },
       delete = { text = "_" }
     },
+    current_line_blame = true,
     current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
     current_line_blame_opts = {
       virt_text = true,
