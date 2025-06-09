d69deeddb58106e3cbe65818050ae4aca5e4a0ba
konata9 <maochichen@163.com>
2025-06-09 20:28:56 +0800

    update config

--- custom/nvim/lua/core/options.lua
+++ custom/nvim/lua/core/options.lua
@@ -69,14 +69,3 @@ function CustomFoldText()
   return string.sub(line, 1, 50) -- Keep 50 chars
 end
 
--- Neovide clipboard
-if vim.g.neovide then
-  vim.o.guifont = "Maple Mono NF CN:h14"
-
-  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
-  vim.keymap.set('v', '<D-c>', '"+y')    -- Copy
-  vim.keymap.set('n', '<D-v>', '"+P')    -- Paste normal mode
-  vim.keymap.set('v', '<D-v>', '"+P')    -- Paste visual mode
-  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
-  vim.keymap.set('i', '<D-v>', '<C-R>+') -- Paste insert mode
-end
